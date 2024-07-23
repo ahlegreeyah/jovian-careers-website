1bd6e4e485ae2171f73a2e8b164972e1bc2c086e
ahlegreeyah <joyisaac.nemi14@gmail.com>
2024-07-23 10:25:58 +0100

    jobpages: format and navigation

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
 import json
+from database import load_jobs_from_db, load_job_from_db
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     jobs = load_jobs_from_db()
-    return render_template('home.html', jobs=jobs, company_name = 'Jovian')
+    return render_template('home.html', jobs=jobs)
 
 @app.route("/api/jobs")
 def list_jobs():
@@ -19,8 +19,12 @@ def list_jobs():
 
 @app.route("/job/<id>")
 def show_job(id):
-    load_job_from_db(id)
-    return jsonify(job)
+    job = load_job_from_db(id)
+
+    if not job:
+        return 'Not Found', 404
+
+    return render_template("jobpage.html", job=job)
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', debug = True)
